9fec8004ce36a8afb85ec7a79bf7d38ba5dea059
Юлия <jumasol@rambler.ru>
2026-05-31 22:03:09 +0300

    macaroons to Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductType } from 'src/types/product.type';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,78 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'macaroons';
+  public advantages = [
+    {
+      image_size: ' width="20px" height="42px"',
+      y: 'y="42.0619999999999px"',
+      number: '1',
+      title: 'Лучшие продукты',
+      description: 'Мы честно готовим макаруны только из натуральных и качественных продуктов. Мы не используем консерванты, ароматизаторы и красители.'
+    },
+    {
+      image_size: ' width="29px" height="43px"',
+      y: 'y="43.0719999999999px"',
+      number: '2',
+      title: 'Много вкусов',
+      description: ' Наша задача – предоставить вам широкое разнобразие вкусов. Вы удивитесь, но у нас более 70 вкусов пироженок.'
+    },
+    {
+      image_size: ' width="31px" height="43px"',
+      y: 'y="43.0719999999999px"',
+      number: '3',
+      title: 'Бисквитное тесто',
+      description: 'Все пирожные готовятся на бисквитном тесте с качественным сливочным маслом 82,5%. В составе нет маргарина и дрожжей! '
+    },
+    {
+      image_size: ' width="34px" height="42px"',
+      y: 'y="42.0619999999999px"',
+      number: '4',
+      title: 'Честный продукт',
+      description: 'Вкус, качество и безопасность наших пирогов подтверждена декларацией о соответствии, которую мы получили 22.06.2016 г.'
+    },
+  ]
+
+  public products = [
+    {
+      image: "macaroon1.png",
+      title: "Макарун с малиной",
+      price: '1,70'
+    },
+    {
+      image: "macaroon2.png",
+      title: "Макарун с манго",
+      price: '1,70'
+    },
+    {
+      image: "macaroon3.png",
+      title: "Пирог с ванилью",
+      price: '1,70'
+    },
+    {
+      image: "macaroon4.png",
+      title: "Пирог с фисташкой",
+      price: '1,70'
+    }
+  ]
+
+  public formValues = {
+    productTitle: '',
+    name: '',
+    phone: '',
+  }
+
+  public scrollTo(target: HTMLElement): void {
+    target.scrollIntoView({ behavior: "smooth" });
+  }
+
+  public addToCard(product: ProductType, target: HTMLElement): void {
+    this.scrollTo(target);
+    this.formValues.productTitle = product.title.toUpperCase();
+
+  }
+
+  public showPresent: boolean = false;
+  public phone:string="375 (29) 368-98-68";
+  public instagram: string = 'Мы в Instagram';
+
 }
